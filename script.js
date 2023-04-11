25fe223444cba920e9c7c7c194aa346f4281ee71
kunnu16 <90325945+kunnu16@users.noreply.github.com>
2023-04-11 20:06:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+window.datalayer = window.datalayer || [];
+
+const myFunction1 = () => {
+    window.datalayer.push({
+        Event : "add To Card Product1 clicked"
+    })
+}
+const myFunction2 = () => {
+    window.datalayer.push({
+        Event : "add To Card Product2  clicked"
+    })
+}
+const myFunction3 = () => {
+    window.datalayer.push({
+        Event : "add To Card  Product3 clicked"
+    })
+}
+const myFunction4 = () => {
+    window.datalayer.push({
+        Event : "add To Card Product4 clicked"
+    })
+}
