d3b205e16dbf8c3366ce4211aed8edff38df364a
Kunal Parmar <kparmar@grepruby.io>
2023-04-12 11:15:50 +0530

    updated task

---
+++ script.js
@@ -0,0 +1,24 @@
+window.datalayer = window.datalayer || [];
+
+const myFunction1 = () => {
+    window.datalayer.push({
+        event : "add To Card Product1 clicked"
+    })
+}
+const myFunction2 = () => {
+    window.datalayer.push({
+        event : "add To Card Product2  clicked"
+    })
+}
+const myFunction3 = () => {
+    window.datalayer.push({
+        event : "add To Card  Product3 clicked"
+    })
+}
+const myFunction4 = () => {
+    window.datalayer.push({
+        event : "add To Card Product4 clicked"
+    })
+}
+
+
